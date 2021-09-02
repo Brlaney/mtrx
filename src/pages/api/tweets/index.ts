bb9699ad8c9aa371def09ff00bc12374b6ddb5ce
brlaney42 <brlaney@outlook.com>
2021-09-02 09:15:01 -0500

    Deleted moved content from app

--- src/pages/api/tweets/index.ts
+++
@@ -1,6 +0,0 @@
-﻿import type { NextApiRequest, NextApiResponse } from 'next'
-import { tweets } from './data'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(tweets)
-}
