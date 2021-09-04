2ad87a4fd3f27267c99114e8fea3c40e64e6d9f7
brlaney42 <brlaney@outlook.com>
2021-09-04 13:43:20 -0500

    s

--- src/components/forms/truss/StepTwo.tsx
+++ src/components/forms/truss/StepTwo.tsx
@@ -10,6 +10,7 @@ import styles from '@/styles/components/Steps.module.scss'
 
 const StepTwo = (props) => {
   const [currentUnits] = React.useState(props.data.units);
+  const [numNodes, setNumNodes] = React.useState(props.data.nodes);
   const [forceUnits, setForceUnits] = React.useState('');
   const [lengthUnits, setLengthUnits] = React.useState('');
 
@@ -18,37 +19,37 @@ const StepTwo = (props) => {
       setForceUnits('lbs');
       setLengthUnits('in');
     }
-  
+
     if (currentUnits === 'imperial2') {
       setForceUnits('lbs');
       setLengthUnits('ft');
     }
-  
+
     if (currentUnits === 'imperial3') {
       setForceUnits('kips');
       setLengthUnits('in');
     }
-  
+
     if (currentUnits === 'imperial4') {
       setForceUnits('kips');
       setLengthUnits('ft');
     }
-  
+
     if (currentUnits === 'metric1') {
       setForceUnits('N');
       setLengthUnits('cm');
     }
-  
+
     if (currentUnits === 'metric2') {
       setForceUnits('N');
       setLengthUnits('m');
     }
-  
+
     if (currentUnits === 'metric3') {
       setForceUnits('KN');
       setLengthUnits('cm');
     }
-  
+
     if (currentUnits === 'metric4') {
       setForceUnits('KN');
       setLengthUnits('m');
@@ -70,7 +71,7 @@ const StepTwo = (props) => {
 
   return (
     <>
-      
+
       {/* Dotnav container for component render */}
       <div className={styles.dotnavContainer}>
         <Dotnav step={props.step} />
@@ -91,18 +92,20 @@ const StepTwo = (props) => {
                 Step 2. <span className={styles.span}>Specify units</span>
               </h2>
 
-              {/* Input units for length */}
-              <div className='uk-width-1-3'>
-                <h6 className={styles.field}>Length/coordinates</h6>
-              </div>
-              <div className='uk-width-2-3'>
-                <Field
-                  name='length'
-                  id={styles.input}
-                  className='uk-radio'
-                  // component={SelectOne}
-                />
-              </div>
+              {/* Input coordinates for each node */}
+              {props.data.nodes.map((key, i) => (
+                <>
+                  <div key={i} className='uk-width-1-1'>
+                    <h6 className={styles.field}>Coordinates for member {i}</h6>
+                    <Field
+                      name='length'
+                      id={styles.input}
+                      className='uk-radio'
+                      // component={SelectOne}
+                    />
+                  </div>
+                </>
+              ))}
 
               {/* Input units for length */}
               <div className='uk-width-1-3'>
@@ -113,7 +116,7 @@ const StepTwo = (props) => {
                   name='length'
                   id={styles.input}
                   className='uk-radio uk-margin'
-                  // component={SelectOne}
+                // component={SelectOne}
                 />
               </div>
 
