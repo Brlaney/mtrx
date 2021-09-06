5085a36ca609ff1c38cf46bdc5f45a0963fd4bc6
brlaney <brlaney@outlook.com>
2021-09-06 15:55:11 -0500

    added truss svg animation

--- src/components/animations/Truss.tsx
+++ src/components/animations/Truss.tsx
@@ -44,43 +44,144 @@ const Truss = () => {
             id='truss_system'
             data-name='Truss system 1'
           >
-            <motion.path
-              {...mainPathVariant}
-              id='element'
-              data-name='Path 1'
-              d="M355 176H388V163H355V176Z"
-              // fill="url(#paint0_linear)"
-              fill={color5}
-              stroke={color5}
-              stroke-width='0.5'
+            <motion.line
+              id='m16'
+              data-name='Path 27'
+              x1='20'
+              y1='163.5'
+              x2='53'
+              y2='163.5'
+              stroke={color1}
+              fill={color2}
+              {...shapeVariations}
               animate='visible'
               initial='hidden'
-              whileHover={{ scale: 1.3 }}
-              whileTap={{ scale: 1.1 }}
             />
             <motion.line
               {...mainPathVariant}
               id='element'
               data-name='Path 1'
-              x1="355"
-              y1="162.5"
-              x2="388"
-              y2="162.5"
-              // fill={color5}
-              stroke={color5}
+              x1='355'
+              y1='162.5'
+              x2='388'
+              y2='162.5'
+              stroke={color1}
               stroke-width='0.5'
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='member1'
+              data-name='Path 21'
+              d='M40 145.85L120.5 83.9251L201 22'
+              stroke='#252323'
+              fill={color2}
+              {...shapeVariations}
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='member2'
+              data-name='Path 24'
+              d='M207 21L287.5 21L368 21'
+              stroke='#252323'
+              fill={color2}
+              {...shapeVariations}
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='member3'
+              data-name='Path 17'
+              d='M371 145V84.5V24'
+              stroke='#252323'
+              fill={color2}
+              {...shapeVariations}
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='member4'
+              data-name='Path 19'
+              d='M207 148H287.5H368'
+              stroke='#252323'
+              fill={color2}
+              {...shapeVariations}
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='member5'
+              data-name='Path 20'
+              d='M40 148H120.5H201'
+              stroke='#252323'
+              fill={color2}
+              {...shapeVariations}
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='member6'
+              data-name='Path 22'
+              d='M204 144V84V24'
+              stroke='#252323'
+              fill={color2}
+              {...shapeVariations}
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='member7'
+              data-name='Path 18'
+              d='M206 145.85L287 83.9251L368 22'
+              stroke='#252323'
+              fill={color2}
+              {...shapeVariations}
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='member8'
+              data-name='Path 23'
+              d='M207 22L287.556 83.9251L368.112 145.85'
+              stroke='#252323'
+              fill={color2}
+              {...shapeVariations}
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='support1'
+              data-name='Path 25'
+              d='M30.2883 162.875L37 151.25L43.7117 162.875H30.2883Z'
+              fill='#CADEFC'
+              stroke='black'
+              stroke-width='0.25'
+              {...shapeVariations}
+              animate='visible'
+              initial='hidden'
+            />
+            <motion.path
+              id='support2'
+              data-name='Path 16'
+              d='M366.043 157.375L371 150.219L375.957 157.375H366.043Z'
+              fill='#CADEFC'
+              stroke='black'
+              stroke-width='0.25'
+              {...shapeVariations}
               animate='visible'
               initial='hidden'
             />
             <motion.circle
               id='support1'
               data-name='Path 3'
-              cx="366"
-              cy="160"
-              r="1.75"
+              cx='366'
+              cy='160'
+              r='1.75'
+              {...shapeVariations}
               fill={color4}
               stroke={color5}
-              stroke-width='0.5'
+              stroke-width='0.25'
               animate={{
                 y: 0,
                 opacity: 1,
@@ -91,191 +192,231 @@ const Truss = () => {
             <motion.circle
               id='supoort2'
               data-name='Path 2'
-              cx="376"
-              cy="160"
-              r="1.75"
+              cx='376'
+              cy='160'
+              r='1.75'
+              {...shapeVariations}
               transform='rotate(-180 284 55)'
               fill={color4}
               stroke={color5}
-              stroke-width='0.5'
+              stroke-width='0.25'
               animate={{
                 y: 0,
                 opacity: 1,
-                transition: { duration: 1.2, ease: [0.6, -0.05, 0.01, 0.99] }
+                transition: {
+                  duration: 1.2,
+                  ease: [0.6, -0.05, 0.01, 0.99]
+                }
               }}
               initial={{ y: 60, opacity: 0, }}
             />
             <motion.circle
-              id='supoort3'
+              id='supoort2'
               data-name='Path 2'
-              cx="371"
-              cy="160"
-              r="1.75"
+              cx='371'
+              cy='160'
+              r='1.75'
+              {...shapeVariations}
               transform='rotate(-180 284 55)'
               fill={color4}
               stroke={color5}
-              stroke-width='0.5'
+              stroke-width='0.25'
               animate={{
                 y: 0,
                 opacity: 1,
-                transition: { duration: 1.2, ease: [0.6, -0.05, 0.01, 0.99] }
+                transition: {
+                  duration: 1.2,
+                  ease: [0.6, -0.05, 0.01, 0.99]
+                }
               }}
               initial={{ y: 60, opacity: 0, }}
             />
             <motion.circle
               id='node1'
               data-name='Path 2'
-              cx="204"
-              cy="147.5"
-              r="3.5"
-              stroke="black"
+              cx='204'
+              cy='147.5'
+              r='3.5'
+              {...shapeVariations}
+              stroke='black'
               transform='rotate(-180 284 55)'
               fill={color4}
               stroke-width='0.5'
               animate={{
                 y: 0,
                 opacity: 1,
-                transition: { duration: 1.2, ease: [0.6, -0.05, 0.01, 0.99] }
+                transition: {
+                  duration: 1.2,
+                  ease: [0.6, -0.05, 0.01, 0.99]
+                }
               }}
               initial={{ y: 60, opacity: 0, }}
             />
             <motion.circle
               id='node2'
               data-name='Path 2'
-              cx="371"
-              cy="147.5"
-              r="3.5"
-              fill="#CADEFC"
-              stroke="black"
+              cx='371'
+              cy='147.5'
+              r='3.5'
+              {...shapeVariations}
+              fill='#CADEFC'
+              stroke='black'
               transform='rotate(-180 284 55)'
               stroke-width='0.5'
               animate={{
                 y: 0,
                 opacity: 1,
-                transition: { duration: 1.2, ease: [0.6, -0.05, 0.01, 0.99] }
+                transition: {
+                  duration: 1.2,
+                  ease: [0.6, -0.05, 0.01, 0.99]
+                }
               }}
               initial={{ y: 60, opacity: 0, }}
             />
             <motion.circle
               id='node3'
               data-name='Path 2'
-              cx="371"
-              cy="20.5"
-              r="3.5"
-              fill="#CADEFC"
-              stroke="black"
+              cx='371'
+              cy='20.5'
+              r='3.5'
+              {...shapeVariations}
+              fill='#CADEFC'
+              stroke='black'
               stroke-width='0.5'
               animate={{
                 y: 0,
                 opacity: 1,
-                transition: { duration: 1.2, ease: [0.6, -0.05, 0.01, 0.99] }
+                transition: {
+                  duration: 1.2,
+                  ease: [0.6, -0.05, 0.01, 0.99]
+                }
               }}
               initial={{ y: 60, opacity: 0, }}
             />
             <motion.circle
               id='node4'
               data-name='Path 2'
-              cx="204"
-              cy="20.5"
-              r="3.5"
-              fill="#CADEFC"
-              stroke="black"
+              cx='204'
+              cy='20.5'
+              r='3.5'
+              {...shapeVariations}
+              fill='#CADEFC'
+              stroke='black'
               stroke-width='0.5'
               animate={{
                 y: 0,
                 opacity: 1,
-                transition: { duration: 1.2, ease: [0.6, -0.05, 0.01, 0.99] }
+                transition: {
+                  duration: 1.2,
+                  ease: [0.6, -0.05, 0.01, 0.99]
+                }
               }}
               initial={{ y: 60, opacity: 0, }}
             />
             <motion.circle
               id='node5'
               data-name='Path 2'
-              cx="37"
-              cy="147.5"
-              r="3.5"
-              fill="#CADEFC"
-              stroke="black"
+              cx='37'
+              cy='147.5'
+              r='3.5'
+              {...shapeVariations}
+              fill='#CADEFC'
+              stroke='black'
               stroke-width='0.5'
               animate={{
                 y: 0,
                 opacity: 1,
-                transition: { duration: 1.2, ease: [0.6, -0.05, 0.01, 0.99] }
+                transition: {
+                  duration: 1.2,
+                  ease: [0.6, -0.05, 0.01, 0.99]
+                }
               }}
               initial={{ y: 60, opacity: 0, }}
             />
             <motion.path
-              id='m1'
-              data-name='Path 12'
-              d="M362 163L356 169"
+              id='m17'
+              data-name='Path 28'
+              d='M27 164L22 170'
+              stroke='#888888'
+              stroke-width='0.5'
               fill={color2}
               {...shapeVariations}
               animate='visible'
               initial='hidden'
             />
             <motion.path
-              id='m2'
-              data-name='Path 13'
-              d="M385 163L379 169"
+              id='m20'
+              data-name='Path 31'
+              d='M34 164L29 170'
+              stroke='#888888'
+              stroke-width='0.5'
               fill={color2}
               {...shapeVariations}
               animate='visible'
               initial='hidden'
             />
             <motion.path
-              id='m3'
-              data-name='Path 14'
-              d="M377 163L371 169"
+              id='m19'
+              data-name='Path 30'
+              d='M41 164L36 170'
+              stroke='#888888'
+              stroke-width='0.5'
               fill={color2}
               {...shapeVariations}
               animate='visible'
               initial='hidden'
             />
             <motion.path
-              id='m4'
-              data-name='Path 15'
-              d="M369 163L363 169"
+              id='m18'
+              data-name='Path 29'
+              d='M48 164L43 170'
+              stroke='#888888'
+              stroke-width='0.5'
               fill={color2}
               {...shapeVariations}
               animate='visible'
               initial='hidden'
             />
             <motion.path
-              id='Fx1_vector'
-              data-name='Path 16'
-
-              stroke={color5}
+              id='m17'
+              data-name='Path 32'
+              d='M362 163L357 169'
+              stroke='#888888'
               stroke-width='0.5'
+              fill={color2}
               {...shapeVariations}
               animate='visible'
               initial='hidden'
             />
             <motion.path
-              id='Fx2_vector'
-              data-name='Path 17'
-
-              stroke={color5}
+              id='m18'
+              data-name='Path 33'
+              d='M369 163L364 169'
+              stroke='#888888'
               stroke-width='0.5'
+              fill={color2}
               {...shapeVariations}
               animate='visible'
               initial='hidden'
             />
             <motion.path
-              id='Fx3_vector'
-              data-name='Path 18'
-
-              stroke={color5}
+              id='m19'
+              data-name='Path 34'
+              d='M376 163L371 169'
+              stroke='#888888'
               stroke-width='0.5'
+              fill={color2}
               {...shapeVariations}
               animate='visible'
               initial='hidden'
             />
             <motion.path
-              id='Fx4_vector'
-              data-name='Path 19'
-
-              stroke={color5}
+              id='m20'
+              data-name='Path 35'
+              d='M383 163L378 169'
+              stroke='#888888'
               stroke-width='0.5'
+              fill={color2}
               {...shapeVariations}
               animate='visible'
               initial='hidden'
