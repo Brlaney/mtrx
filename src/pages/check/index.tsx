36b5180a9d75bd55313db2fd046607eeaf096382
brlaney42 <brlaney@outlook.com>
2021-09-18 09:31:01 -0500

    f

--- src/pages/check/index.tsx
+++ src/pages/check/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { NextPage } from 'next'
+import { NextPage } from 'next';
 
 interface Props {
   userAgent?: string;
-}
+};
 
 const Check: NextPage<Props> = ({ userAgent }) => (
-  <main>Your user agent: {userAgent}</main>
-)
+  <div className='uk-flex uk-align-center uk-text-justify uk-width-1-2 uk-margin-top'>
+    Your user agent: {userAgent}
+  </div>
+);
 
 Check.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-  return { userAgent }
+  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+  console.log(userAgent);
+  return { userAgent };
 }
 
 export default Check;
