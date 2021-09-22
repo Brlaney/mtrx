4432f2a7c2b024c1b0b1b7c11b7854bd33e541fb
brlaney42 <brlaney@outlook.com>
2021-09-22 13:33:25 -0500

    Pushing unrestrained frame changes

--- src/pages/tests/index.tsx
+++ src/pages/tests/index.tsx
@@ -4,7 +4,6 @@ import { motion } from 'framer-motion';
 import { fadeInUp, stagger } from '@/lib/config/animations/tests/svgs';
 import GoBack from '@/components/global/buttons/GoBack';
 import styles from '@/styles/pages/Tests.module.scss';
-
 import { elementLengths, localK } from '@/lib/utils/matrix/truss';
 
 const Tests = () => {
@@ -26,11 +25,11 @@ const Tests = () => {
   console.log('Each elements orientation in degrees:')
   console.log(testOutput[2]);
 
-  console.log("Each n row maps local k_ij's to global degree of freedom:");
-  console.log(testOutput[3]);
-
   console.log("Index of global degrees of freedom:");
   console.log(testOutput[4]);
+
+  console.log("Each n row maps local k_ij's to global degree of freedom:");
+  console.log(testOutput[3]);
 
   const k = localK(5, testOutput[1], testOutput[0], E, A);
   console.log('These arrays contain a 4 x 4 local stiffness matrix ([k]) for each element in the system.');
