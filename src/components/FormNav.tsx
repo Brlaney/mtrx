5b36e6ce6fe482cd6a756e5e81e5e2f4aaa6ba0d
brlaney42 <brlaney@outlook.com>
2021-11-14 15:13:34 -0600

    aligning text in form nav

--- src/components/FormNav.tsx
+++ src/components/FormNav.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import Full from '@/components/global/form-nav/Full';
 import H from '@/components/global/form-nav/H';
 import ModelIcon from '@/components/global/form-nav/ModelIcon';
-
 import styles from '@/styles/components/FormNav.module.scss';
 
 export default function FormNav() {
@@ -15,23 +14,29 @@ export default function FormNav() {
       <div className={styles.outerbox} />
 
       <div className={styles.stepbox1}>
-        Step 1
+        <div className={styles.text}>
+          Step 1
+        </div>
       </div>
 
       <div className={styles.rodbox1}>
-        <Full lhs={1} rhs={0} stage={1} />
+        <H lhs={1} rhs={0} stage={1} />
       </div>
 
       <div className={styles.stepbox2}>
-        Step 2
+        <div className={styles.text}>
+          Step 2
+        </div>
       </div>
 
       <div className={styles.rodbox2}>
-        <Full lhs={0} rhs={0} stage={0} />
+        <H lhs={0} rhs={0} stage={0} />
       </div>
 
       <div className={styles.stepbox3}>
-        Step 3
+        <div className={styles.text}>
+          Step 3
+        </div>
       </div>
 
       <div className={styles.rodbox3}>
@@ -43,12 +48,12 @@ export default function FormNav() {
       </div>
 
       <div className={styles.modelbox}>
-        Analyze your model
+        <div className={styles.text}>
+          Analyze your model
+        </div>
       </div>
 
-      <div className={styles.outerbox}>
-        Outer RHS
-      </div>
+      <div className={styles.outerbox} />
 
     </div>
   )
