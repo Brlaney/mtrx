d9881f5a28ddf42030ec695d3eec96f7a9d1f78b
brlaney42 <brlaney@outlook.com>
2022-02-19 13:13:48 -0600

    f

--- src/lib/utils/viewport/index.ts
+++ src/lib/utils/viewport/index.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function useWindowSize() {
   // Initialize state with undefined width/height (matches server/client renders)
@@ -20,13 +20,13 @@ export default function useWindowSize() {
       }
 
       // Add event listener
-      window.addEventListener("resize", handleResize);
+      window.addEventListener('resize', handleResize);
 
       // Call handler right away so state gets updated with initial window size
       handleResize();
 
       // Remove event listener on cleanup
-      return () => window.removeEventListener("resize", handleResize);
+      return () => window.removeEventListener('resize', handleResize);
     }
   }, []); // Empty array ensures that effect is only run on mount
   
